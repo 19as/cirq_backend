4d90958ff6b2f4768d06b1ffbd9492eb81f53e7b
whoa2ht <whoa2ht@gmail.com>
2024-10-18 18:51:02 +0300

    non-measured qasm
    flags and save update

--- solver.py
+++ solver.py
@@ -1,22 +1,27 @@
 import cirq
 import click
 from cirq.contrib.qasm_import import circuit_from_qasm
+import json
 
 @click.command()
 @click.argument('qasm', type=click.Path(exists=True))
 @click.argument('shots', type=int)
-# @click.option('-c', '--chart', is_flag=True, help='Build chart')
-# @click.option('-s', '--save', nargs=2, help='Path and filename to save')
+@click.option('-c', '--chart', is_flag=True, help='Build chart')
+@click.option('-s', '--save', nargs=2, help='Path and filename to save')
 @click.option('-sh', '--show', is_flag=True, help='Print result to console')
 
-def main(qasm, shots, show) -> dict:
+def main(qasm, shots, show, save, chart) -> dict:
     with open(qasm, 'r') as f:
         qasm_str = f.read()
     circuit = circuit_from_qasm(qasm_str)
     circuit = measures(circuit)
     counts = process(circuit, shots)
     if show:
-        print(counts.histogram(key = 1))
+        print(counts.data)
+    if chart:
+        print(circuit)
+    if save:
+        counts.data.to_json(save, orient='records')
     print('done!')
 
 def measures(circuit):
